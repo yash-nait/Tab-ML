c5bfc0888e3fbe897913ac49a89ff5839f7bb145
yash-nait <yashnait@gmail.com>
2021-06-02 09:10:43 +0530

    add classification

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import ( Flask, render_template, request)
 import os
 
-# ML
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split 
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, OrdinalEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
+from sklearn import metrics
 
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestRegressor
+# ML
+from models.regression import reg_predict
+from models.classification import cls_predict
 
 app = Flask(__name__)
 
@@ -22,48 +16,6 @@ UPLOAD_FOLDER = dir_path+"//static"
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() == "csv"
-
-def predict(train_file, test_file):
-    try:
-        train = pd.read_csv(UPLOAD_FOLDER+"//"+train_file)
-        X_train = train.drop(["target"], axis=1)
-        y_train = train["target"].copy()
-        num = []
-        cat = []
-        for att in X_train.columns.values:
-            if X_train[att].dtype == np.dtype('O'):
-                cat.append(att)
-            else :
-                num.append(att)
-        num_pipeline = Pipeline([
-            ("imputer", SimpleImputer(strategy="median")),
-            ("std_scalar", StandardScaler())
-        ])
-        cat_pipeline = Pipeline([
-            ("imputer_cat", SimpleImputer(strategy="most_frequent")),
-            ("encode", OrdinalEncoder())
-        ])
-
-        full_pipeline = ColumnTransformer([
-            ("num", num_pipeline, num),
-            ("cat", cat_pipeline, cat)
-        ])
-
-        data_prepared = full_pipeline.fit_transform(X_train)
-
-        rand_tree_reg = RandomForestRegressor()
-
-        rand_tree_reg.fit(data_prepared, y_train)
-
-        X_test = full_pipeline.fit_transform(pd.read_csv(UPLOAD_FOLDER+"//"+test_file))
-
-        pred = rand_tree_reg.predict(X_test)
-
-        pd.DataFrame(pred).to_csv(UPLOAD_FOLDER+"//prediction//prediction.csv", index=False)
-        return True
-    except Exception as e:
-        print("err: "+str(e))
-        return False
 
 @app.route("/")
 def home_page():
@@ -86,18 +38,41 @@ def regression():
             train_file.save(train_location)
             test_file.save(test_location)
 
-            pred = predict(train_file.filename, test_file.filename)
+            (pred, pos_err, score) = reg_predict(train_file.filename, test_file.filename, UPLOAD_FOLDER)
             
             if pred :
-                return render_template("regressor.html", file_name="prediction.csv")
+                return render_template("result.html", file_name="prediction.csv", score=score, metrics="Root Mean Square Error")
             else :
-                return render_template("error.html")
+                return render_template("error.html", err=pos_err)
         else:
-            return render_template("error.html" , err="file err")
+            return render_template("error.html" , err="file error")
     return render_template("regression.html")
 
-@app.route("/classification")
+@app.route("/classification", methods=["GET", "POST"])
 def classification():
+    if request.method == "POST":
+        train_file = request.files["train"]
+        test_file = request.files["test"]
+        if (train_file and test_file) and (allowed_file(train_file.filename) and allowed_file(test_file.filename)):
+            train_location = os.path.join(
+                UPLOAD_FOLDER,
+                train_file.filename
+            )
+            test_location = os.path.join(
+                UPLOAD_FOLDER,
+                test_file.filename
+            )
+            train_file.save(train_location)
+            test_file.save(test_location)
+
+            (pred, pos_err, score) = cls_predict(train_file.filename, test_file.filename, UPLOAD_FOLDER)
+            
+            if pred :
+                return render_template("result.html", file_name="prediction.csv", score=score, metrics="Accuracy")
+            else :
+                return render_template("error.html", err=pos_err)
+        else:
+            return render_template("error.html" , err="file error")
     return render_template("classification.html")
 
 if __name__ == "__main__":
---
+++ models/classification.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split 
+
+#PREPROCESSING
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+
+#METRICS
+from sklearn.metrics import accuracy_score
+
+#MODELS
+from sklearn.ensemble import RandomForestClassifier
+
+def cls_predict(train_file, test_file, UPLOAD_FOLDER):
+    try:
+        train = pd.read_csv(UPLOAD_FOLDER+"//"+train_file)
+        train, val = train_test_split(train, test_size=0.2, random_state=42)
+        X_train = pd.DataFrame(train).drop(["target"], axis=1)
+        y_train = pd.DataFrame(train)["target"].copy()
+        num = []
+        cat = []
+        for att in X_train.columns.values:
+            if X_train[att].dtype == np.dtype('O'):
+                cat.append(att)
+            else :
+                num.append(att)
+        num_pipeline = Pipeline([
+            ("imputer", SimpleImputer(strategy="median")),
+            ("std_scalar", StandardScaler())
+        ])
+        cat_pipeline = Pipeline([
+            ("imputer_cat", SimpleImputer(strategy="most_frequent")),
+            ("encode", OrdinalEncoder())
+        ])
+
+        full_pipeline = ColumnTransformer([
+            ("num", num_pipeline, num),
+            ("cat", cat_pipeline, cat)
+        ])
+
+        data_prepared = full_pipeline.fit_transform(X_train)
+
+        rand_tree_cls = RandomForestClassifier(random_state=42)
+
+        rand_tree_cls.fit(data_prepared, y_train)
+
+        #accuracy score
+        X_val = pd.DataFrame(val).drop(["target"], axis=1)
+        y_val = pd.DataFrame(val)["target"].copy()
+        X_val = full_pipeline.fit_transform(X_val)
+        acc = accuracy_score(rand_tree_cls.predict(X_val), y_val)*100
+
+        X_test = full_pipeline.fit_transform(pd.read_csv(UPLOAD_FOLDER+"//"+test_file))
+
+        pred = rand_tree_cls.predict(X_test)
+
+        pd.DataFrame(pred).to_csv(UPLOAD_FOLDER+"//prediction//prediction.csv", header=['prediction'], index=False)
+        return (True, "", acc)
+    except Exception as e:
+        print("err: "+str(e))
+        return (False, str(e), np.nan)
